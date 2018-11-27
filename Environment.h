f9d4bec3009e999245984fe47e21bf0a887bef94
zobrien99 <33132309+zobrien99@users.noreply.github.com>
2018-11-27 18:10:25 -0500

    Update Environment.h

--- Environment.h
+++ Environment.h
@@ -1,4 +1,13 @@
+/* 
+Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
+Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
+Date: 11/27/18
+Class: EEEE-346-01
+Assignment: Project 3
 
+Purpose:
+ 
+*/
 
 #pragma once
 #include <vector>
