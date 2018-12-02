aacb24b78f0b457fcde902679c98e4aad4d05b0a
PhaseCycle <45368224+PhaseCycle@users.noreply.github.com>
2018-12-01 21:39:41 -0500

    Update Sinusoid.h

--- Sinusoid.h
+++ Sinusoid.h
@@ -5,26 +5,25 @@ Date: 12/1/18
 Class: EEEE-346-01
 Assignment: Project 3
 
-Purpose:
- 
+Purpose: sinusoid manipulation done in seperate class so as to describe sinfunction as only needing time to perform calculation
 */
+#pragma once
 
 class Sinusoid{
     
     private:
     
-    double amp, per, phase;
-    
+        double amp, per, phase;
     
     public:
-    double get_amp();
-    void set_amp(double, double);
-    double get_per();
-    void set_per(double);
-    double get_phase();
-    void set_phase(double);
+        double get_amp();
+        void set_amp(double, double);
+        double get_per();
+        void set_per(double);
+        double get_phase();
+        void set_phase(double);
     
-    double func(double);
+        double func(double);
     
     
 };
