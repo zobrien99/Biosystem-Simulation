cf656e48a0630af3fc9afe0a66317d2031e29bb6
tstryjski <38745154+tstryjski@users.noreply.github.com>
2018-12-01 22:01:40 -0500

    Add files via upload

---
+++ MicroOrganism.h
@@ -0,0 +1,23 @@
+/*
+Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
+Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
+Date: 12/1/18
+Class: EEEE-346-01
+Assignment: Project 3
+
+Purpose: derived class for organisms in micro-enviornments
+
+*/
+#pragma once
+
+#include "Organism.h"
+#include "Point_3D.h"
+
+class MicroOrganism :public Organism {
+	protected:
+		Point_3D p3;
+	public:
+	
+	Point_3D getLocation();
+	void setLocation(double x_new, double y_new, double z_new);
+};
