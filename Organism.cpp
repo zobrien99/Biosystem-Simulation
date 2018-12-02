3f0d217c8f66283390034ece2df7e11999e2d578
PhaseCycle <45368224+PhaseCycle@users.noreply.github.com>
2018-12-01 20:45:04 -0500

    Update Organism.cpp

--- Organism.cpp
+++ Organism.cpp
@@ -1,3 +1,15 @@
+/* 
+Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
+Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
+Date: 12/1/18
+Class: EEEE-346-01
+Assignment: Project 3
+
+Purpose: Executes functions declared in Organism.h
+ 
+*/
+
+
 #include "Organism.h"
 #pragma once
 
