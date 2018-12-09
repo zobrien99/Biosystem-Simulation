1f4da5d3d676d47dae690d0ee37a005fd4503330
Zachary O'Brien <zjo5244@rit.edu>
2018-12-09 21:59:29 +0000

    Added to Fungus

---
+++ Fungus.cpp
@@ -0,0 +1,66 @@
+/*
+Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
+Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
+Date: 12/9/18
+Class: EEEE-346-01
+Assignment: Project 3
+
+Purpose: Implementation of derived class for small fungi.
+*/
+
+#include "Fungus.h"
+#include <cmath>
+
+Fungus::Fungus(double x, double y, double z) {
+	consumption_amount = 0;
+	consumption_time = 0;
+	reproduction_amount = 5;
+	reproduction_time = 1;
+	age = 0;
+	movement = 2;
+	spawn_distance = 4;
+	visibility = 1;
+
+	setLocation(x, y, z);
+}
+
+//getters
+//none needed
+
+//setters
+void Fungus::set_sunlight(double x) {
+	sunlight = x;
+}
+void Fungus::set_chemical(double x) {
+	chemical = x;
+}
+void Fungus::set_fertility() {
+	fertility = 1 + ((2 * sunlight - 2 * chemical) / 3);
+}
+
+//other
+
+double Fungus::fRand(double fMin, double fMax) {
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
+}
+
+void Fungus::reproduce(Organism *O) {
+	double theta = fRand(0, 2 * 3.14159265);
+	double phi = fRand(0, 2 * 3.14159265);
+
+	double x = spawn_distance * cos(phi) * cos(theta);
+	double y = spawn_distance * cos(phi) * sin(theta);
+	double z = spawn_distance * sin(phi);
+
+	O = new Fungus(x, y, z);
+
+}
+void Fungus::aged(int a) {
+	movement = movement - (a/20); //decereases movement the older the cell is, although this is scaled by the constant 10. Accumulates
+	spawn_distance = 4 - (a / 100);//decreases spawn distance as the cell gets older, scaled by the constant 100. Acculmulates
+}
+
+
+
+
---
+++ Fungus.h
@@ -0,0 +1,30 @@
+/*
+Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
+Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
+Date: 12/8/18
+Class: EEEE-346-01
+Assignment: Project 3
+
+Purpose: derived class for small fungi
+*/
+
+#include "MicroOrganism.h"
+#pragma once
+
+class Fungus:public MicroOrganism {
+
+	protected:
+		double fertility;        
+        
+    public:
+        Fungus(double x, double y, double);
+        void set_sunlight(double x);
+        void set_chemical(double x);
+        void set_fertility();
+        void reproduce();
+        void aged(int AGE);
+        double fRand(double fMin, double fMax);    	
+
+
+};
+
