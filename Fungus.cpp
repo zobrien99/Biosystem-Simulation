be40981b017752221e8330a7b0e57bd0998337a3
zobrien99 <33132309+zobrien99@users.noreply.github.com>
2018-12-09 17:06:09 -0500

    Fixed githubs weird formatting.

--- Fungus.cpp
+++ Fungus.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 Authors: Andrew Yoder, Thomas Stryjski, Zachary O'Brien
 Email: aby7159@rit.edu, tgs9181@rit.edu, zjo5244@rit.edu
@@ -61,12 +61,4 @@ void Fungus::aged(int a) {
 	movement = movement - (a/20); //decereases movement the older the cell is, although this is scaled by the constant 10. Accumulates
 	spawn_distance = 4 - (a / 100);//decreases spawn distance as the cell gets older, scaled by the constant 100. Acculmulates
 }
-
-=======
-
-
-#include "Fungus.h"
->>>>>>> da62becf1712bb9db3e2381da485c666b5ab44f2
-
-
 
