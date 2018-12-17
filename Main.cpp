c712a0624c17f557741d0802ea59bc667ce0a36f
PhaseCycle <45368224+PhaseCycle@users.noreply.github.com>
2018-12-16 22:14:17 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -22,6 +22,9 @@ using namespace std;
 
 
 int main() {
+	//set random number generator back to intial value
+	srand(1);
+
 	int cond, ani_pop, plant_pop, bact_pop, fung_pop, t;
 	double macEnv_x, macEnv_y, micEnv_x, micEnv_y;
 	double micEnv_z, min_t, max_t;
